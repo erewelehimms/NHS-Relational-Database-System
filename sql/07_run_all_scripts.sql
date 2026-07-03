c0d1758e415f61dd2d182a537911f4cd2bec11d9
erewelehimms <erewelehimms@gmail.com>
2026-07-03 22:22:37 +0500

    Create 07_run_all_scripts.sql

---
+++ sql/07_run_all_scripts.sql
@@ -0,0 +1,10 @@
+-- Run the files in this order inside MySQL Workbench:
+-- 1. sql/01_create_database_and_tables.sql
+-- 2. sql/02_insert_sample_data.sql
+-- 3. sql/03_advanced_queries.sql
+-- 4. sql/04_stored_procedure.sql
+-- 5. sql/05_trigger_and_audit.sql
+-- 6. sql/06_security_roles_and_hashing.sql
+
+-- This file is only an instruction file.
+-- MySQL Workbench users should open each SQL file and run them one by one.
